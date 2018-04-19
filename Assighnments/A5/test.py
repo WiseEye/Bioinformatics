8b6fff34c9a949ba9aa40446aee751c4218d007a
Daniel Heinen <dh9621@mcla.edu>
2018-04-19 17:53:09 +0000

    a5

---
+++ Assighnments/A5/test.py
@@ -0,0 +1,7 @@
+import bioinformatics
+
+
+
+print bioinformatics.translate_dna("ATGTTCGGT") 
+print bioinformatics.translate_dna("ACAACCAGT") 
+print bioinformatics.translate_dna("GACTCCTTTGAGGGA") 
